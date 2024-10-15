929465d881f3ccc37ec57de4c86359a3dbebbc35
Michael J <30031129@cc.peralta.edu>
2024-10-14 19:23:35 -0700

    used smart pointers for dynamic array management

--- userInput.cpp
+++ userInput.cpp
@@ -5,6 +5,32 @@ using namespace std;
 
 namespace input{
     void userInput(){
-        
+        int size = 1;
+        //int* numbers = new int[size];
+        unique_ptr<int[]> numbers = make_unique<int[]>(size);
+        int entries = 0;
+
+
+        while(true){
+            cout << "number: " << endl;
+            cin >> numbers[entries];
+            if(cin.fail()){
+                entries--;
+                break;
+            }
+            entries++;
+            if(entries == size){
+                size *= 2;
+                unique_ptr<int[]> temporaryArray = make_unique<int[]>(size);
+                for(int i = 0; i < entries; i++){
+                    temporaryArray[i] = numbers[i];
+                }
+                numbers = make_unique<int[]>(size);
+                for(int i = 0; i < entries; i ++){
+                    numbers[i] = temporaryArray[i];
+                }
+                
+            }
+        }
     }
 }
