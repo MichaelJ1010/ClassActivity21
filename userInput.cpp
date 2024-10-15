58015058027a412ece90059ab546a89e42ef78ea
Michael J <30031129@cc.peralta.edu>
2024-10-14 19:07:00 -0700

    added hpp and cpp file for userInput function

---
+++ userInput.cpp
@@ -0,0 +1,10 @@
+#include "./userInput.hpp"
+#include <iostream>
+
+using namespace std;
+
+namespace input{
+    void userInput(){
+        
+    }
+}
---
+++ userInput.hpp
@@ -0,0 +1,8 @@
+#ifndef USERINPUT_H
+#define USERINPUT_H
+
+namespace input{
+    void userInput();
+}
+
+#endif
